87e48aee7d968fdc319538f8f3565980c28958c9
WangWilly <s89667654@gmail.com>
2025-04-30 14:08:26 +0800

    refactor: 💡 test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 # This script is used to run the test suite for the project.
 # It sets up the environment and runs the tests.
@@ -9,4 +9,20 @@ if [ ! -f go.mod ]; then
   echo "Please run this script from the root of the project."
   exit 1
 fi
-go test ./...
+
+declare -a test_list=()
+while read pkg; do 
+  pkg_dir=$(go list -f '{{.Dir}}' $pkg)
+  if [ ! -f "$pkg_dir/skip_test.go" ]; then 
+    test_list+=("$pkg")
+  else
+    echo "Skipping tests in $pkg"
+  fi
+done < <(go list ./...)
+
+# echo "Running tests in the following packages:"
+# for pkg in "${test_list[@]}"; do
+#   echo " - $pkg"
+# done
+
+go test -cover -covermode=atomic "${test_list[@]}" || exit 1
